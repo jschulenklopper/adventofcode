62b1418da34dd104a7c234af0371b94de2f51d2c
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2015-12-19 13:01:19 +0100

    Store temp solution, in order to start refactoring.

--- day07/day07.rb
+++ day07/day07.rb
@@ -4,46 +4,46 @@ class String
   end
 end
 
+class Instructions < Array
+end 
+
 class Instruction
   attr_reader :operation, :in_1, :in_2, :out
   
-  def to_s
-    puts "%s %s %s -> %s" % [@in_1, @operation, @in_2, @out]
-  end
-
   def Instruction.parse(string)
-    left = /(.+) \-\>/.match(string)[1]
-    op, in_1, in_2 = Instruction.parse_left(left)
-    @operation = op # ? Operation.new(op) : nil
-    @in_1 = (in_1 && in_1.is_integer?) ? in_1.to_i : (in_1 ? Wire.new(in_1) : nil)
-    @in_2 = (in_2 && in_2.is_integer?) ? in_2.to_i : (in_2 ? Wire.new(in_2) : nil)
+    pattern = /(.+) \-\> (\w+)/
 
-    right = /\-\> (\w+)/.match(string)[1]
-    out = Instruction.parse_right(right)
+    @operation, @in_1, @in_2 = Instruction.parse_left(pattern.match(string)[1])
+    @out = Instruction.parse_right(pattern.match(string)[2])
 
     Instruction.new(@operation, @in_1, @in_2, @out)
   end
 
   def Instruction.parse_right(string)
-    @out = Wire.new(string)
+    string
+  end
+
+  def Instruction.value_or_wire(string)
+    string.is_integer? ? string.to_i : string
   end
 
   def Instruction.parse_left(string)
-    biop_string = /(.+) (AND|OR|LSHIFT|RSHIFT) (.+)/
-    uniop_string = /(NOT) (.+)/
-    nop_string = /(\w+)/
+    biop_pattern = /(.+) (AND|OR|LSHIFT|RSHIFT) (.+)/
+    uniop_pattern = /(NOT) (.+)/
+    nop_pattern = /(\w+)/
 
-    if string =~ biop_string
-      op = biop_string.match(string)[2]
-      in_1 = biop_string.match(string)[1]
-      in_2 = biop_string.match(string)[3]
-    elsif string =~ uniop_string
-      op = uniop_string.match(string)[1]
-      in_1 = uniop_string.match(string)[2]
+    if string =~ biop_pattern
+      op = biop_pattern.match(string)[2]
+      in_1 = Instruction.value_or_wire(biop_pattern.match(string)[1])
+      in_2 = Instruction.value_or_wire(biop_pattern.match(string)[3])
+    elsif string =~ uniop_pattern
+      op = uniop_pattern.match(string)[1]
+      in_1 = Instruction.value_or_wire(uniop_pattern.match(string)[2])
       in_2 = nil
-    elsif string =~ nop_string
-      op, in_2 = "assign", nil
-      in_1 = nop_string.match(string)[1]
+    elsif string =~ nop_pattern
+      op = nil
+      in_1 = Instruction.value_or_wire(nop_pattern.match(string)[1])
+      in_2 = nil
     end
 
     [op, in_1, in_2]
@@ -58,19 +58,7 @@ class Instruction
 end
 
 class Operation
-  attr_reader :op
-
-  def to_s
-    @op.to_s
-  end 
-
-  def initialize(op)
-    if %w(OR LSHIFT RSHIFT AND NOT).include?(op)
-      @op = op.downcase
-    else
-      @op = "assign"
-    end
-  end
+  attr_writer :op
   
   def not(in_1)
   end
@@ -88,37 +76,12 @@ class Operation
   end
 end
 
-class Value
-  attr_reader :value
-
-  def Value.is_value?(string)
-    return false if string =~ /[a-z]/
-    true
-  end
-
-  def initialize(value)
-    @value = value
-  end
-end
-
-class Wire
-  attr_reader :id
-  attr_writer :signal
-
-  def initialize(id)
-    @id = id
-  end
-end
-
-wires = [];
-instructions = [];
+instructions = Instructions.new;
 
 # Parse the wiring instructions
 while line = gets
   puts line
   instructions << Instruction.parse(line)
-  p instructions.last
-  puts 
 end
- 
-puts instructions.length
+
+puts "---\nNumber of instructions: %d" % [instructions.length]
