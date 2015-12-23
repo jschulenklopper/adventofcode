69221e693e162370b3de6c0d2137715dd2195284
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2015-12-23 23:45:28 +0100

    Add solution for day 15, second part.

---
+++ day15/day15b.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+class Ingredient
+  def method_missing(property, value)
+    instance_variable_set(("@" + property.to_s).to_sym, value.to_s)
+  end
+end
+
+def distribute(total, ingredients)
+  yield ({ingredients.first => total}) and return if ingredients.length == 1
+
+  ingredient = ingredients.first
+  (0..total).each do |amount|
+    distribute(total - amount, ingredients - [ingredient] ) do |distribution|
+      yield ({ingredient=>amount}.merge(distribution))
+    end
+  end
+end
+
+def compute_score(combination)
+  score = 1
+
+  %w(capacity durability flavor texture).each do |property|
+    subscore = 0
+    calorie_score = 0
+    combination.each do |ingredient, amount|
+      subscore += amount * ingredient.instance_variable_get("@#{property}").to_i
+      calorie_score += amount * ingredient.instance_variable_get("@calories").to_i
+    end
+    subscore = 0 if subscore < 0 or calorie_score != 500
+    score *= subscore
+  end
+  score
+end
+
+ingredients = Array.new
+
+while line = gets
+  pattern = /(\w+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)/
+
+  ingredient = Ingredient.new
+
+  %w(capacity durability flavor texture calories).each_with_index do |property, i|
+    ingredient.send(property, pattern.match(line)[i+2].to_i)
+  end
+
+  ingredients << ingredient
+end
+
+c = Array.new
+max_score = 0
+
+distribute(100, ingredients) do |combination|
+  score = compute_score(combination)
+  max_score = score if score > max_score
+  c << combination
+end
+
+
+puts max_score
