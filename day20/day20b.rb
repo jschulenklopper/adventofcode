fc8ad1eb0e766594f3ada618555ef98dd80cca8e
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2015-12-25 22:31:44 +0100

    Add solutions for day 20.

---
+++ day20/day20b.rb
@@ -0,0 +1,26 @@
+target = 36000000
+
+def visiting_elves(house)
+  factors = []
+  max = Math.sqrt(house)
+  1.upto(max) do |f|
+    factors.concat([f,house/f]) if house % f == 0
+  end
+  factors.uniq
+end
+
+house = 0
+number_of_presents = 0
+until number_of_presents >= target
+  house += 1
+  number_of_presents = 0
+
+  elves = visiting_elves(house)
+  elves.each do |elf|
+    if house <= 50 * elf
+      number_of_presents += elf * 11
+    end
+  end
+end
+
+puts house
