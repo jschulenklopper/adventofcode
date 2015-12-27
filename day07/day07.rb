4be87a0979bdd4631ee3572ed25b7a7b1e6d9cc3
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2015-12-27 19:45:10 +0100

    Another temp commit to save current state. Doesn't work, for reasons not yet clear.

--- day07/day07.rb
+++ day07/day07.rb
@@ -5,22 +5,32 @@ class String
 end
 
 class Instructions < Array
+  def value(wire)
+    puts "value(%s)" % wire
+    puts "     (%s)" % wire.class
+    if wire.class == Integer || wire.class == Fixnum
+      return wire
+    end
+    if wire.is_integer?
+      return wire.to_i
+    else
+      p instruction = self.find { |instr| instr.out == wire }
+      p op = instruction.operation
+      p in_1 = value(instruction.in_1)
+      p in_2 = value(instruction.in_2) if instruction.in_2
+      return eval("Instruction.%s(%s,%s)" % [op, in_1, in_2])
+    end
+  end
 end 
 
 class Instruction
   attr_reader :operation, :in_1, :in_2, :out
   
-  def Instruction.parse(string)
+  def initialize(string)
     pattern = /(.+) \-\> (\w+)/
 
     @operation, @in_1, @in_2 = Instruction.parse_left(pattern.match(string)[1])
-    @out = Instruction.parse_right(pattern.match(string)[2])
-
-    Instruction.new(@operation, @in_1, @in_2, @out)
-  end
-
-  def Instruction.parse_right(string)
-    string
+    @out = pattern.match(string)[2]
   end
 
   def Instruction.value_or_wire(string)
@@ -41,7 +51,7 @@ class Instruction
       in_1 = Instruction.value_or_wire(uniop_pattern.match(string)[2])
       in_2 = nil
     elsif string =~ nop_pattern
-      op = nil
+      op = "ASSIGN"
       in_1 = Instruction.value_or_wire(nop_pattern.match(string)[1])
       in_2 = nil
     end
@@ -49,30 +59,34 @@ class Instruction
     [op, in_1, in_2]
   end
   
-  def initialize(operation, in_1, in_2, out)
-    @operation = operation
-    @in_1 = in_1
-    @in_2 = in_2
-    @out = out
-  end
-end
-
-class Operation
-  attr_writer :op
-  
-  def not(in_1)
+  def Instruction.ASSIGN(in_1, *in_2)
+    puts "Instruction.ASSIGN(): %s" % in_1
+    in_1
   end
 
-  def or(in_1, in_2)
+  def Instruction.NOT(in_1, *in_2)
+    puts "Instruction.NOT(): %s" % ~in_1
+    ~in_1
   end
 
-  def and(in_1, in_2)
+  def Instruction.OR(in_1, in_2)
+    puts "Instruction.OR(): %s" % (in_1 | in_2)
+    in_1 | in_2
   end
 
-  def rshift(in_1, in_2)
+  def Instruction.AND(in_1, in_2)
+    puts "Instruction.AND(): %s" % (in_1 & in_2)
+    in_1 & in_2
   end
 
-  def lshift(in_1, in_2)
+  def Instruction.RSHIFT(in_1, in_2)
+    puts "Instruction.RSHIFT(): %s" % (in_1 << in_2)
+    in_1 << in_2
+  end
+
+  def Instruction.LSHIFT(in_1, in_2)
+    puts "Instruction.LSHIFT(): %s" % (in_1 >> in_2)
+    in_1 >> in_2
   end
 end
 
@@ -81,7 +95,10 @@ instructions = Instructions.new;
 # Parse the wiring instructions
 while line = gets
   puts line
-  instructions << Instruction.parse(line)
+  instructions << Instruction.new(line)
 end
 
 puts "---\nNumber of instructions: %d" % [instructions.length]
+
+# Compute value of requested wire.
+puts instructions.value("a")
