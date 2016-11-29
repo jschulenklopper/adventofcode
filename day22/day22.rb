f59225d9674cb1e44901d9b0304bb61a1a0a9d6d
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-11-29 08:52:10 +0100

    Temp commit not-yet-working solution.

--- day22/day22.rb
+++ day22/day22.rb
@@ -1,87 +1,191 @@
-shop_inventory = [
-  {name: "Magic Missile", cost: 53, damage: 4},
-  {name: "Drain", cost: 73, damage: 2, hitpoints: 2},
+# Read input file for boss.
+hitpoints = 13 # DEBUG /(?<hitpoints>\d+)/.match(gets)[:hitpoints].to_i
+damage = 8 # DEBUG /(?<damage>\d+)/.match(gets)[:damage].to_i
+
+SHOP_INVENTORY = [
+  {name: "Magic Missile", cost: 53, damage: 4, duration: 1},       # Added duration.
+  {name: "Drain", cost: 73, damage: 2, hitpoints: 2, duration: 1}, # Added duration.
   {name: "Shield", cost: 113, armor: 7, duration: 6},
   {name: "Poison", cost: 173, damage: 3, duration: 6},
   {name: "Recharge", cost: 229, mana: 101, duration: 5}
 ]
 
-def buy_spells(inventory)
-  # TODO
-  number_of_items = 0
-  while true
-    number_of_items += 1
-    # Pick a new combination of spells.
-    inventory.combination(number_of_items).each do |serie|
-      puts serie.to_s
+SPELLS_LENGTH = 8 # DEBUG Just a first guess at number of turns necessary.
 
-      # Test whether it is allowed as series.
-      # TODO
+def select_spells(inventory)
+  # Count, represented as string in base 5, is used to generate
+  # all possible combinations of inventory, starting with one item
+  # and adding items along the way.
+  count = 0
 
-      yield serie
+  while count < inventory.length ** SPELLS_LENGTH
+    # Convert count to base 5 number/string.
+    code = count.to_s(inventory.length)
+
+    code = "30" # DEBUG Remove this line - it is to test scripted battle from exercise.
+
+    # Pick selection of spells according to code.
+    spells = code.each_char.map { |char| inventory[char.to_i] }
+
+    yield spells
+
+    # Increase count for code of next combination.
+    count += 1
+  end
+end
+
+def select_spell(actor, victim, spells, turn)
+  puts "\n>>>"
+  puts "select_spell(%s, %s, %d, %d)" % [actor[:name], victim[:name], spells.length, turn]
+
+  # Select current spell.
+  spell = spells[turn]
+
+  # Check if there's a spell to cast.
+  if not spell
+    puts "  no spell to cast"
+    actor[:hitpoints] = 0
+    return
+  end
+
+  # Check if actor has sufficient mana to cast spell.
+  if actor[:mana] < spell[:cost]
+    puts "  not enough mana to cast spell"
+    actor[:hitpoints] = 0
+    return
+  end
+
+  # Check if spell may be cast (no similar spell still running).
+  if turn > 0 && spells[0 .. turn-1].select { |s| s[:name] == spell[:name] && s[:duration] > 0}.length > 0
+    puts "  spell may not be cast yet"
+    actor[:hitpoints] = 0
+    return
+  end
+
+  # Deduct mana necessary for spell that is cast in this turn.
+  actor[:mana] -= spell[:cost]
+  actor[:mana_used] += spell[:cost]
+
+  puts "  selected spell: %s" % spell.to_s
+
+  return spell
+end
+
+def use_spell(actor, victim, spells, turn)
+  puts "\n>>>"
+  puts "use_spell(%s, %s, %d, %d)" % [actor[:name], victim[:name], spells.length, turn]
+
+  puts "  list of spells:"
+  puts spells
+
+  # For the spell cast, compute effect and reduce duration.
+  puts "  compute effect of all spells"
+
+  spells[0..turn].each do |spell|
+
+    puts "   considering spell %s" % spell.to_s
+
+    # For all running spells...
+    if spell[:duration] > 0
+
+      puts "   applying spell %s" % [spell[:name]]
+
+      case spell[:name]
+        when "Magic Missile" # {name: "Magic Missile", cost: 53, damage: 4},
+          puts "    damage %d" % spell[:damage]
+          victim[:hitpoints] -= spell[:damage]
+
+        when "Drain" # {name: "Drain", cost: 73, damage: 2, hitpoints: 2},
+          puts "    damage %d" % spell[:damage]
+          victim[:hitpoints] -= spell[:damage]
+          puts "    hitpoints %d" % spell[:hitpoints]
+          actor[:hitpoints] += spell[:hitpoints]
+
+        when "Shield" # {name: "Shield", cost: 113, armor: 7, duration: 6},
+          puts "    armor %d" % spell[:armor]
+          actor[:armor] = spell[:armor] # sic
+
+        when "Poison" # {name: "Poison", cost: 173, damage: 3, duration: 6},
+          puts "    damage %d" % spell[:damage]
+          victim[:hitpoints] -= spell[:damage]
+
+        when "Recharge" # {name: "Recharge", cost: 229, mana: 101, duration: 5}
+          puts "    mana %d" % spell[:mana]
+          actor[:mana] += spell[:mana]
+      end
+
+      # Lower duration of spell.
+      spell[:duration] -= 1
     end
   end
 end
 
-def cast_spell(actor, victim, cart)
-end
+def attack(actor, victim, spells, turn)
+  puts "\n<<<"
+  puts "attack(%s, %s, %d, %d)" % [actor[:name], victim[:name], spells.length, turn]
 
-def update_cart(cart)
-  return cart.clone
-end
+  # Compute armor of victim (from running spells).
+  armor = 0
 
-def attack(actor, victim)
   # Compute damage.
-  damage = [1, actor[:damage] - victim[:armor]].max
+  armor = victim[:armor] ? victim[:armor] : 0
+  damage = [1, actor[:damage] - armor].max
 
   # Apply damage, reduce hitpoints.
   victim[:hitpoints] -= damage
 end
 
-# Read input file for boss.
-hitpoints = /(?<hitpoints>\d+)/.match(gets)[:hitpoints].to_i
-damage = /(?<damage>\d+)/.match(gets)[:damage].to_i
-
 winning_sequence = Hash.new
 lowest_mana = nil
 
 # Create valid combinations and see if it results in a win.
-buy_spells(shop_inventory) do |spells|
+select_spells(SHOP_INVENTORY) do |spells|
+  puts "\n====="
+  puts spells
+
   # Set up boss and me for start of new battle.
-  boss = {name: "boss", hitpoints: hitpoints, damage: damage}
-  me = {name: "me", hitpoints: 50, mana: 500}
+  # DEBUG boss = {name: "boss", hitpoints: hitpoints, damage: damage}
+  boss = {name: "boss", hitpoints: 13, damage: 8}
+  # DEBUG me = {name: "me", hitpoints: 50, mana: 500, mana_used: 0}
+  me = {name: "me", hitpoints: 10, mana: 250, mana_used: 0}
 
-  current_spells = spells.clone
+  turn = 0
 
-  next
+  puts "   me: HP %d, M %d (%d), boss: HP %d, D %d" % [me[:hitpoints], me[:mana], me[:mana_used], boss[:hitpoints], boss[:damage]]
 
-  while true 
-    cast_spell(me, boss, current_spells)
-    break if boss[:hitpoints] <= 0
-    current_spells = update_spells(current_spells)
-
-    attack(boss, me)
+  while true
+    # My turn
+    select_spell(me, boss, spells, turn)
+    puts "-> me: HP %d, M %d (%d), boss: HP %d, D %d" % [me[:hitpoints], me[:mana], me[:mana_used], boss[:hitpoints], boss[:damage]]
     break if me[:hitpoints] <= 0
+
+    # use_spell(me, boss, spells, turn)
+    # puts "-> me: HP %d, M %d (%d), boss: HP %d, D %d" % [me[:hitpoints], me[:mana], me[:mana_used], boss[:hitpoints], boss[:damage]]
+    # break if boss[:hitpoints] <= 0
+
+    use_spell(me, boss, spells, turn)
+    puts "-> me: HP %d, M %d (%d), boss: HP %d, D %d" % [me[:hitpoints], me[:mana], me[:mana_used], boss[:hitpoints], boss[:damage]]
+    break if me[:hitpoints] <= 0
+
+    # Boss' turn
+    attack(boss, me, spells, turn)
+    puts "-> me: HP %d, M %d (%d), boss: HP %d, D %d" % [me[:hitpoints], me[:mana], me[:mana_used], boss[:hitpoints], boss[:damage]]
+    break if boss[:hitpoints] <= 0
+
+    turn += 1
   end
 
   # Determine if I won.
-  if me[:hitpoints] > 0
-    # Calculate costs of spells, and remember if it is lowest.
-    mana = calculate_cost(spells)
-    lowest_mana ||= manas
-    lowest_mana = mana if mana < lowest_mana
+  if me[:hitpoints] > 0 && boss[:hitpoints] <= 0
+    puts "\n=> I won"
+    # Get amount of mana used, and remember if it is lowest.
+    mana_used = me[:mana_used]
+    lowest_mana ||= mana_used
+    lowest_mana = mana_used if mana_used < lowest_mana
+  else
+    puts "\n=> I lost"
   end
+
 end
 
 puts lowest_mana
-
-# try_spell(available_spells, past_spells) => boolean
-#   available_spells.each do |spell|
-#     see if spell fits with past_spells
-#     play round:
-#       cast_spell
-#       check if boss dead => return true
-#       receive attack
-#       check if I'm dead => return false
-#     return try_spell(available_spells, past_spells + spell)
-
