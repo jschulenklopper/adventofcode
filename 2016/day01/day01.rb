062a13b3ba8f60925d808b49ac655b653386fbf4
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-01 22:55:28 +0100

    Minor improvements to day 1. day01.rb contains solution for both parts.

--- 2016/day01/day01.rb
+++ 2016/day01/day01.rb
@@ -1,19 +1,17 @@
 position = [0, 0]  # Starting position
 orientation = 0 # North is 0, East is 1, South is 2, West is 3
 
-while line = gets do # Assume that instructions span multiple lines.
-  instructions = line.strip.split(", ")
-  
-  instructions.each do | instruction |
-    turn, distance = instruction[0], instruction[1..-1].to_i
-    turn == "R" ? orientation += 1 : orientation += -1
+instructions = gets.strip.split(", ")
 
-    case orientation % 4
-      when 0 then position[1] += distance
-      when 1 then position[0] += distance
-      when 2 then position[1] -= distance
-      when 3 then position[0] -= distance
-    end
+instructions.each do | instruction |
+  turn, distance = instruction[0], instruction[1..-1].to_i
+  turn == "R" ? orientation += 1 : orientation += -1
+
+  case orientation % 4
+    when 0 then position[1] += distance
+    when 1 then position[0] += distance
+    when 2 then position[1] -= distance
+    when 3 then position[0] -= distance
   end
 end
 
--- 2016/day01/day01b.rb
+++ 2016/day01/day01b.rb
@@ -1,29 +1,30 @@
 position = [0, 0]  # Starting position.
 orientation = 0 # North is 0, East is 1, South is 2, West is 3.
-visited = []
+visited = [] # Array of locations visited before.
+second_time = nil
 
-while line = gets do # Assume that instructions can span multiple lines.
-  instructions = line.strip.split(", ")
-  
-  instructions.each do | instruction |
-    turn, distance = instruction[0], instruction[1..-1].to_i
-    turn == "R" ? orientation += 1 : orientation += -1
+instructions = gets.strip.split(", ")
 
-    while distance > 0
-      case orientation % 4
-        when 0 then position[1] += 1 # Move North.
-        when 1 then position[0] += 1 # Move East.
-        when 2 then position[1] -= 1 # 
-        when 3 then position[0] -= 1
-      end
+instructions.each do | instruction |
+  turn, distance = instruction[0], instruction[1..-1].to_i
+  turn == "R" ? orientation += 1 : orientation += -1
 
-      if visited.include? (position)
-        puts position[0].abs + position[1].abs
-        exit
-      end
-
-      visited << position.dup
-      distance -= 1
+  while distance > 0
+    case orientation % 4
+      when 0 then position[1] += 1 # Move North.
+      when 1 then position[0] += 1 # Move East.
+      when 2 then position[1] -= 1 # South.
+      when 3 then position[0] -= 1 # West
     end
+
+    if visited.include?(position) and second_time == nil
+      second_time = position.dup
+    end
+
+    visited << position.dup
+    distance -= 1
   end
 end
+
+puts "Final distance: %s" % [position[0].abs + position[1].abs]
+puts "First location visited twice: %d" % [second_time[0].abs + second_time[1].abs]
