1dc45c04afe91a212c1a4d0ba662a4b9fe7aeda2
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-02 09:17:46 +0100

    Add solution for day 2, first part.

---
+++ 2016/day02/day02.rb
@@ -0,0 +1,17 @@
+directions = { "U" => -3, "D" => +3, "L" => -1, "R" => +1 }
+
+digit = 5  # Starting digit.
+code = ""
+
+while instructions = gets do
+  instructions.strip.chars.each do | instruction |
+    # Compute next possible digit...
+    candidate = digit + directions[instruction]
+    # ... but test whether it is a valid one.
+    digit = candidate if candidate.between?(1,9)
+  end
+
+  code << digit.to_s
+end
+
+puts code
