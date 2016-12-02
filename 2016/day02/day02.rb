904a1260fbaadb2cabb7d420a3925a84f2a35f63
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-02 14:35:36 +0100

    Improve test to not go out of bounds.

--- 2016/day02/day02.rb
+++ 2016/day02/day02.rb
@@ -8,7 +8,13 @@ while instructions = gets do
     # Compute next possible digit...
     candidate = digit + directions[instruction]
     # ... but test whether it is a valid one.
-    digit = candidate if candidate.between?(1,9)
+    if instruction == "L" && ! [1,4,7].include?(digit) ||
+       instruction == "R" && ! [3,6,9].include?(digit) ||
+       instruction == "U" && ! [1,2,3].include?(digit) ||
+       instruction == "D" && ! [7,8,9].include?(digit) 
+    then
+      digit = candidate if candidate.between?(1,9)
+    end
   end
 
   code << digit.to_s
