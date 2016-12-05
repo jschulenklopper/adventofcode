cbf7c44814342c26329f0b0f01f7b4c99fcd1559
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-05 09:40:49 +0100

    Remove commented instruction.

--- 2016/day05/day05b.rb
+++ 2016/day05/day05b.rb
@@ -7,7 +7,7 @@ start = 0
 until password.length == 8
   hash = Digest::MD5.hexdigest(input + start.to_s)
   if hash[0,5] == "00000" && hash[5].between?("0","7")
-    position = hash[5].to_i # if hash[5].between?("0","7")
+    position = hash[5].to_i
     character = hash[6]
 
     if ! password.has_key?(position)
