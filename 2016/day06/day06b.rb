be00e4faceea76f1288c94fc79da0f1c17e5e658
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-06 10:46:58 +0100

    Add solution for day 6.

---
+++ 2016/day06/day06b.rb
@@ -0,0 +1,16 @@
+characters = []  # Array of possible characters, for each position stored in hash.
+message = ""
+
+while line = gets do
+  line.strip.each_char.with_index { |c, i|
+    characters[i] = {} if ! characters[i]
+    characters[i][c] = 0 if ! characters[i][c]
+    characters[i][c] += 1
+  }
+end
+
+characters.each do |possibilities|
+  message << possibilities.min_by { |k,v| v }.first
+end
+
+puts message
