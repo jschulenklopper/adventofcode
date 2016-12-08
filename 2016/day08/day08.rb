e2cfe22b7b2709b13ec5f0cc53effe4024cee296
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-08 12:03:27 +0100

    Print screen after instructions, so answer can be read.

--- 2016/day08/day08.rb
+++ 2016/day08/day08.rb
@@ -1,25 +1,20 @@
-ROWS = 6
-COLUMNS = 50
+ROWS, COLUMNS = 6, 50 
 screen = Array.new(ROWS) { Array.new(COLUMNS, ".") } 
 
 while line = gets do
   if /rect (?<x>\d+)x(?<y>\d+)/ =~ line.strip
-    (0...y.to_i).each do |row|
-      (0...x.to_i).each do |col|
-        screen[row][col] = "#"
-      end
-    end
+    (0...y.to_i).each { |row| (0...x.to_i).each { |col| screen[row][col] = "#" } } 
   elsif /rotate row y=(?<row>\d+) by (?<delta>\d+)/ =~ line.strip
-    delta.to_i.times do 
-      screen[row.to_i] = screen[row.to_i].rotate(-1)
-    end
+    delta.to_i.times { screen[row.to_i] = screen[row.to_i].rotate(-1) }
   elsif /rotate column x=(?<column>\d+) by (?<delta>\d+)/ =~ line.strip
-    screen = screen.transpose
-    delta.to_i.times do 
-      screen[column.to_i] = screen[column.to_i].rotate(-1)
-    end
-    screen = screen.transpose
+    screen = screen.transpose  # Cheap trick to address columns as rows.
+    delta.to_i.times { screen[column.to_i] = screen[column.to_i].rotate(-1) }
+    screen = screen.transpose  # And transpose it back.
   end
 end
 
 puts screen.join { |row| row.join }.count("#")
+
+screen.each do |row|
+  puts row.join
+end
