54a345f049595bfbb2577d4455f999a0fbe7693d
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-14 10:19:13 +0100

    Version of 14.1 without debug statements. Still slow though.

--- 2016/day14/day14.rb
+++ 2016/day14/day14.rb
@@ -5,30 +5,23 @@ salt = gets.strip
 nr_of_keys = 0
 index = 0
 
-memo = []  # Memoization of hashes.
-
 until nr_of_keys == 64
-  puts "hash: %s (at index %d)" % [hash = Digest::MD5.hexdigest(salt + index.to_s), index]
+  hash = Digest::MD5.hexdigest(salt + index.to_s)
 
   if match = hash.match(/(\w)\1{2}/) # Three same characters in a row.
-    char = match[1]
-    puts "X three subsequent %s at index %s" % [char, index]
     char = match[1]
 
     # Go forward at most 1000 hashes.
     forward_index = index
-    
     found = false
 
     until found || (forward_index >= index + 1000)  do
       forward_index += 1
       # Can we see this again?
-      puts "  forward_hash: %s (at forward_index %d)" % [forward_hash = Digest::MD5.hexdigest(salt + forward_index.to_s), forward_index]
+      forward_hash = Digest::MD5.hexdigest(salt + forward_index.to_s)
 
       if forward_match = forward_hash.match("%s{5}" % char)
-        puts "  X five subsequent %s at forward_index %s (index %d)" % [char, forward_index, index]
         nr_of_keys += 1
-        puts "  keys found so far: %s (index %s, forward_index %d)" % [nr_of_keys, index, forward_index]
         break
       end
     end
@@ -37,4 +30,4 @@ until nr_of_keys == 64
   index += 1
 end  
 
-puts index - 1
+puts index - 1  # Minus 1 because we increase index once too often.
