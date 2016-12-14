438f7c14bf9cf1130a39b09b807fa448ed0a4a98
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-14 13:04:22 +0100

    Add memoization to 14.1.

--- 2016/day14/day14.rb
+++ 2016/day14/day14.rb
@@ -3,10 +3,13 @@ require 'digest'
 salt = gets.strip
 
 nr_of_keys = 0
-index = 0
+index = -1
+
+memo = []
 
 until nr_of_keys == 64
-  hash = Digest::MD5.hexdigest(salt + index.to_s)
+  index += 1
+  hash = memo[index] || memo[index] = Digest::MD5.hexdigest(salt + index.to_s)
 
   if match = hash.match(/(\w)\1{2}/) # Three same characters in a row.
     char = match[1]
@@ -18,7 +21,7 @@ until nr_of_keys == 64
     until found || (forward_index >= index + 1000)  do
       forward_index += 1
       # Can we see this again?
-      forward_hash = Digest::MD5.hexdigest(salt + forward_index.to_s)
+      forward_hash = memo[forward_index] || memo[forward_index] = Digest::MD5.hexdigest(salt + forward_index.to_s)
 
       if forward_match = forward_hash.match("%s{5}" % char)
         nr_of_keys += 1
@@ -26,8 +29,6 @@ until nr_of_keys == 64
       end
     end
   end
-
-  index += 1
 end  
 
-puts index - 1  # Minus 1 because we increase index once too often.
+puts index
