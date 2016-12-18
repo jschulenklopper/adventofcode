66b568ea5b642a9012edd6a0b568c1fcf799f970
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2016-12-18 15:36:35 +0100

    Add solution for day 18.1.

---
+++ 2016/day18/day18.rb
@@ -0,0 +1,26 @@
+def new_row(cur_row)
+  cur_row.chars.each_index.map { |i|
+    left = (i-1 >= 0) ? cur_row[i-1] : "."
+    center = cur_row[i]
+    right = (i+1 <= cur_row.length - 1) ? cur_row[i+1] : "."
+    adjecent = left + center + right
+    if adjecent == "^^." ||  # Its left and center tiles are traps, but its right tile is not.
+       adjecent == ".^^" ||  # Its center and right tiles are traps, but its left tile is not.
+       adjecent == "^.." ||  # Only its left tile is a trap.
+       adjecent == "..^"     # Only its right tile is a trap.
+      "^"
+    else
+      "."
+    end
+  }.join
+end
+
+input = gets.strip
+
+grid = [input]  # Fill grid with first line.
+
+until grid.length == 40
+  grid << new_row(grid.last)
+end
+
+puts grid.join.chars.count(".")
