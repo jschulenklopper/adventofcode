85c265316a167dcefbfc8314f9e34d024140f7c8
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-01 12:48:44 +0100

    Shorter solution for day 1b.

--- 2017/day01/day01.rb
+++
@@ -1,9 +0,0 @@
-while line = gets
-  digits = line.strip.chars.map(&:to_i)
-  sum = 0
-
-  digits.each.with_index do |digit, index| 
-    sum += digit if digit == digits[(index+1) % digits.length]	
-  end
-  puts sum  
-end
--- 2017/day01/day01b.rb
+++ 2017/day01/day01b.rb
@@ -2,8 +2,7 @@ while line = gets
   digits = line.strip.chars.map(&:to_i)
   sum = 0
   digits.each.with_index do |digit, index| 
-    other_digit = (index + digits.length / 2) % digits.length
-    sum += digit if digit == digits[other_digit]	
+    sum += digit if digit == digits[(index + digits.length / 2) % digits.length]
   end
-  puts sum  
+  puts sum
 end
