0f616587418bec9c967dfd7ebf0afedf3075bb93
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-01 16:04:38 +0100

    Add solution for day 1, first part.

---
+++ 2017/day01/day01a.rb
@@ -0,0 +1,8 @@
+while line = gets
+  digits = line.strip.chars.map(&:to_i)
+  sum = 0
+  digits.each.with_index do |digit, index| 
+    sum += digit if digit == digits[(index+1) % digits.length]	
+  end
+  puts sum  
+end
