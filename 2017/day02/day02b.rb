cc45861d6f0683816be60ea63942a2d21511e7fa
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-02 11:09:41 +0100

    Even more concise solution. Happy so far.

--- 2017/day02/day02b.rb
+++ 2017/day02/day02b.rb
@@ -1,9 +1,8 @@
 checksum = 0
 while line = gets
   values = line.strip.split(" ").map(&:to_i)
-  divisors = values.select do |i|
-    values.select { |j| (i % j) == 0 || (j % i) == 0 }.length > 1
-  end
-  checksum += divisors.max / divisors.min
+  checksum += values.product(values).map { |i,j|
+    (i % j == 0 && i != j) ? i / j : nil
+  }.compact.first
 end
 puts checksum  
