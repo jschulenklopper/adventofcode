ae3f7360d310792ab0ed20afada5a17ecaa8b538
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-03 22:42:54 +0100

    Add cleaned version of solution for day 3a.

--- 2017/day03/day03a.rb
+++ 2017/day03/day03a.rb
@@ -3,7 +3,6 @@ def position(n)
   higher_square = Math.sqrt(n).ceil**2
   lower_square = Math.sqrt(n).floor**2
   range_length = Math.sqrt(higher_square).floor
-  puts "   l: %s" % range_length
   
   x, y = nil, nil
   if higher_square.even?
@@ -32,25 +31,5 @@ def distance(position)
   position[0].abs + position[1].abs
 end
 
-(5..9).each do |n|
-# (17..25).each do |n|
-# (10..16).each do |n|
-# (26..36).each do |n|
-  pos = position(n) 
-  puts "n: %s, (%s, %s)" % [n, pos[0], pos[1]]
-  puts "  d: %s" % distance(pos)
-end
-
-raise "distance(16) not correct" if not distance(position(16)) == 3
-raise "distance(15) not correct" if not distance(position(15)) == 2
-raise "distance(14) not correct" if not distance(position(14)) == 3
-raise "distance(13) not correct" if not distance(position(13)) == 4
-
-raise "distance(1) not correct" if not distance(position(1)) == 0
-raise "distance(12) not correct" if not distance(position(12)) == 3
-raise "distance(23) not correct" if not distance(position(23)) == 2
-raise "distance(1024) not correct" if not distance(position(1024)) == 31
-
 n = gets.strip.to_i
-puts "n: %s" % n
 puts distance(position(n))
