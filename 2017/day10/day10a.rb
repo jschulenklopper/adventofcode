3ef1d31bdf9f418dcc0a08ba8ab340fe25d07628
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-10 17:04:14 +0100

    Remove print statements.

--- 2017/day10/day10a.rb
+++ 2017/day10/day10a.rb
@@ -1,8 +1,6 @@
 MAX = 256
 
 def hash(input, start, skip, lengths)
-  puts "hash(%s,\n\t%s, %s,\n\tlengths)" % [input.to_s, start, skip]
-
   lengths.each do |length|
     input = input.rotate(start)     # Rotate string to start.
     rev = input[0, length].reverse  # Part to be reversed.
@@ -20,16 +18,12 @@ end
 
 # Form list, from 0 to MAX
 list = (0...MAX).to_a
-puts "list: %s" % [list.to_s]
 
 # Read lengths from stdin
-p lengths = gets.split(",").map(&:to_i)
-
-puts "lengths: %s" % [lengths.to_s]
+lengths = gets.split(",").map(&:to_i)
 
 current = 0
 skip_size = 0
 
 hashed = hash(list, current, skip_size, lengths)
-puts "hashed: %s" % [hashed.to_s]
 puts hashed[0] * hashed[1]
