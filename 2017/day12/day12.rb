3676d3d93ca183d315cd39ff26c3b734c084fb0c
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-12 13:02:02 +0100

    Style-improved version of day 12 solution.

--- 2017/day12/day12.rb
+++ 2017/day12/day12.rb
@@ -1,39 +1,43 @@
+require 'set'
+
 def reach(programs, id)
   reach = []
 
   # Process id via a breadth-first search (thus a queue).
   queue = [id]
   while node = queue.shift
-    reach << node unless reach.include?(node)
-    programs[node].each { |n| queue.push(n) unless reach.include?(n) }
+    # Add current node to reach.
+    reach << node
+    ## Add connected nodes to queue, unless it's already in reach or in queue.
+    programs[node].each { |n| queue.push(n) unless reach.include?(n) || queue.include?(n)}
   end
 
-  reach
+  # Sorting the items will ignore differences in order.
+  reach.sort
 end
 
-programs = Hash.new(Array.new)
+programs = Hash.new { |k,v| Set.new }
 
+# Build representation of network.
 while line = gets
-  line.match(/^(?<from>\w+) \<-\> (?<to>.+)$/) do |match|
-    from = match[:from].to_i
-    tos = match[:to].split(", ").map(&:to_i)
-
-    tos.each do |to|
-		  programs[from] += [to] unless programs[from].include?(to)
-      # Strictly, this isn't necessary, but it's according to specification.
-      programs[to] += [from] unless programs[to].include?(from)
-    end
-   
+  # Read left and right part from line.
+  from, dest = line.split("<->").map(&:strip)
+  # Add all destinations to `from`, and `from` to all destinations.
+  # Converting the ids to ints speeds things up considerably.
+  dest.split(", ").each do |to|
+    programs[from.to_i] += [to.to_i] 
+    programs[to.to_i] += [from.to_i] # Not really necessary, but it's in spec.
   end
 end
 
-connected = []
-programs.each { |n, _|
-  # Find all the nodes to be reached from n.
-  mst = reach(programs, n).sort
-  # Add that list if it's a new list.
-  connected.push(mst) unless connected.include?(mst)
+# Build groups of connected programs.
+groups = Set.new
+programs.each { |node, _list|
+  # Find all the nodes to be reached from node.
+  group = reach(programs, node)
+  # Add that list to groups (but only if it's a new one; via the set).
+  groups.add(group)
 }
 
-puts "size of group %s: %s" % [0, connected[0].length]
-puts "number of groups: %s" % connected.length
+puts "size of group %s: %s" % [0, reach(programs, 0).length]
+puts "number of groups: %s" % groups.length
