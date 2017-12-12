a7f1af6807fe3250c215163371fe10a034d2d501
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-12 09:56:05 +0100

    A bit better, but bigger improvements on their way.

--- 2017/day12/day12.rb
+++ 2017/day12/day12.rb
@@ -1,36 +1,39 @@
-$programs = Hash.new(Array.new)
+def reach(programs, id)
+  reach = []
 
-def bfc(id)
-  all = []
+  # Process id via a breadth-first search (thus a queue).
   queue = [id]
-
-  while node = queue.pop
-    all << node unless all.include?(node)
-    $programs[node].each { |n| queue.push(n) unless all.include?(n) }
+  while node = queue.shift
+    reach << node unless reach.include?(node)
+    programs[node].each { |n| queue.push(n) unless reach.include?(n) }
   end
 
-  return all
+  reach
 end
+
+programs = Hash.new(Array.new)
 
 while line = gets
   line.match(/^(?<from>\w+) \<-\> (?<to>.+)$/) do |match|
     from = match[:from].to_i
-    to = match[:to].split(", ").map(&:to_i)
+    tos = match[:to].split(", ").map(&:to_i)
 
-    to.each do |program|
-		  $programs[from] += [program] unless $programs[from].include?(program)
-      $programs[program] += [from] unless $programs[program].include?(from)
+    tos.each do |to|
+		  programs[from] += [to] unless programs[from].include?(to)
+      # Strictly, this isn't necessary, but it's according to specification.
+      programs[to] += [from] unless programs[to].include?(from)
     end
    
   end
 end
 
-puts "size of group %s: %s" % [0, bfc(0).length]
-
-msts = []
-$programs.each { |n, _|
-  mst = bfc(n)
-  msts.push(mst.sort) unless msts.include?(mst.sort)
+connected = []
+programs.each { |n, _|
+  # Find all the nodes to be reached from n.
+  mst = reach(programs, n).sort
+  # Add that list if it's a new list.
+  connected.push(mst) unless connected.include?(mst)
 }
 
-puts msts.length
+puts "size of group %s: %s" % [0, connected[0].length]
+puts "number of groups: %s" % connected.length
