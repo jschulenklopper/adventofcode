9e9bc8083e022614c43f8113349de28a694069f1
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-23 10:00:40 +0100

    New attempt, another bug fix done.

--- 2017/day23/ruby.rb
+++ 2017/day23/ruby.rb
@@ -1,16 +1,18 @@
+a,b,c,d,e,f,g,h = 0,0,0,0,0,0,0,0
+
 b = 99
 c = 99
 b *= 100
 b += 100000
 c = b
 c += 17000
-# b = 109900
-# c = 126900
 
 loop do  # label2:
+  puts "  label2"
   f = 1
   d = 2
   loop do  # label5:
+    puts "    label5"
     e = 2
     loop do # label4:
       g = d
@@ -18,8 +20,8 @@ loop do  # label2:
       g -= b
       if g == 0
         f = 0
-        e += 1
       end
+      e += 1
       g = e
       g -= b
       break if g == 0
@@ -28,6 +30,7 @@ loop do  # label2:
     g = d
     g -= b
     break if g == 0
+    # puts "a: 1, b: %s, c: %s, d: %s, e: %s, f: %s, g: %s, h: %s" % [b,c,d,e,g,f,h]
   end
   if f == 0
     h += 1
@@ -35,6 +38,9 @@ loop do  # label2:
   end
   g = b
   g -= c
-  if g == 0 then puts h; exit end
+  if g == 0
+    puts h
+    exit
+  end
   b += 17
 end
