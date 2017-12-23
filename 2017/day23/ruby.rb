8996c9668f96d271fee04caeae371c91271b79f7
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-23 09:20:32 +0100

    Correct typos. Add loop.

--- 2017/day23/ruby.rb
+++ 2017/day23/ruby.rb
@@ -1,41 +1,37 @@
-a = 1
 b = 99
-c = b
-if a != 0 goto label1
-goto label2
-label1:
+c = 99
 b *= 100
-b -= 100000
+b += 100000
 c = b
-c -= 17000
-label2:
-f = 1
-d = 2
-label5:
-e = 2
-label4:
-g = d
-g *= e
-g -= b
-if g != 0 goto label3
-f = 0
-e -= 1
-label3:
-g = e
-g -= b
-if g != 0 goto label4
-d -= 1
-g = d
-g -= b
-if g != 0 goto label5
-if f != 0 goto label6
-h -= -1
-label6:
-g = b
-g -= c
-if g != 0 goto label7
-goto exit
-label7:
-b -= 17
-goto label2
-exit:
+c += 17000
+# b = 109900
+# c = 126900
+
+loop do # label2:
+  f = 1
+  d = 2
+  # label5:
+  e = 2
+  # label4:
+  g = d
+  g *= e
+  g -= b
+  if g != 0 goto label3
+  f = 0
+  e += 1
+  # label3:
+  g = e
+  g -= b
+  if g != 0 goto label4
+  d += 1
+  g = d
+  g -= b
+  if g != 0 goto label5
+  if f != 0 goto label6
+  h += 1
+  # label6:
+  g = b
+  g -= c
+  if g == 0 puts h; exit
+  b += 17
+end
