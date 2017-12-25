3135da542f22ba643facec0d0007287dc4f73520
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2017-12-25 21:20:47 +0100

    Add simple solution for day 25.

---
+++ 2017/day25/day25.rb
@@ -0,0 +1,46 @@
+STEPS = 12523873
+state = "A"
+checksum = 0
+positions = Hash.new(0)
+position = 0
+
+# Return state and position after making move.
+def step(value, position, positions, position_delta, next_state)
+  positions[position] = value
+  position += position_delta
+  return [next_state, position]
+end
+
+STEPS.times do 
+  value = positions[position]
+  situation = [state, value]
+
+  case situation
+    when ["A", 0]
+      state, position = step(1, position, positions, +1, "B")
+    when ["A", 1]
+      state, position = step(1, position, positions, -1, "E")
+    when ["B", 0]
+      state, position = step(1, position, positions, +1, "C")
+    when ["B", 1]
+      state, position = step(1, position, positions, +1, "F")
+    when ["C", 0]
+      state, position = step(1, position, positions, -1, "D")
+    when ["C", 1]
+      state, position = step(0, position, positions, +1, "B")
+    when ["D", 0]
+      state, position = step(1, position, positions, +1, "E")
+    when ["D", 1]
+      state, position = step(0, position, positions, -1, "C")
+    when ["E", 0]
+      state, position = step(1, position, positions, -1, "A")
+    when ["E", 1]
+      state, position = step(0, position, positions, +1, "D")
+    when ["F", 0]
+      state, position = step(1, position, positions, +1, "A")
+    when ["F", 1]
+      state, position = step(1, position, positions, +1, "C")
+  end
+end
+
+puts positions.values.reduce(:+)
