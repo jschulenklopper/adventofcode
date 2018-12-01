6897a1664a5f8aa49200bb037f2411303396b3a1
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-01 16:42:28 +0100

    Improve solution for part 2. Add input.txt.

--- 2018/day01/day01b.rb
+++ 2018/day01/day01b.rb
@@ -1,16 +1,12 @@
 freq = 0
 freqs = []
-deltas = []
-index = 0
 
-while delta = gets
-  deltas << delta.strip.to_i
-end
-
-until freqs.include?(freq)
-  freqs << freq
-  freq += deltas[index % deltas.length]
-  index += 1
+# Get all input, split at newlines, convert to integers,
+# and then cycle through that array indefinitely.
+gets(nil).split.map(&:to_i).cycle do |d|
+  freq += d                      # Adjust frequence.
+  break if freqs.include?(freq)  # Break if seen before.
+  freqs << freq                  # Add frequency to the list.
 end
 
 puts freq
