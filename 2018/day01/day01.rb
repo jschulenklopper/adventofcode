220b5302b6a760a3637d1d1658b1fef28526a468
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-01 16:44:22 +0100

    Update day01.rb

--- 2018/day01/day01.rb
+++ 2018/day01/day01.rb
@@ -1,12 +1,3 @@
 # Get the whole input file, split at newlines,
 # convert to integers, and reduce array by adding all values.
 puts gets(nil).split.map(&:to_i).reduce(0,&:+)
-
-# More straightforward approach:
-# freq = 0  # Start frequency.
-
-# while line = gets
-#   freq += line.strip.to_i  # Add delta to frequency.
-# end
-
-# puts freq
