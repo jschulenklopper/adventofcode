2e75dd314e7dfdb410081b1f12326f435187ea0e
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-01 15:46:17 +0100

    Commit one-liner for part 1.

--- 2018/day01/day01.rb
+++ 2018/day01/day01.rb
@@ -1,7 +1,10 @@
-freq = 0  # Start frequency.
+puts gets(nil).split("\n").map(&:to_i).reduce(0,&:+)
 
-while line = gets
-  freq += line.strip.to_i  # Add delta to frequency.
-end
+# More straightforward approach:
+# freq = 0  # Start frequency.
 
-puts freq
+# while line = gets
+#   freq += line.strip.to_i  # Add delta to frequency.
+# end
+
+# puts freq
