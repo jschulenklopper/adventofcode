cd48470cbb956f6b8495f3f793734f42a2f5c1dd
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-02 20:25:07 +0100

    Improve solution for part 1, add comments.

--- 2018/day02/day02.rb
+++ 2018/day02/day02.rb
@@ -1,12 +1,13 @@
 exactly_two, exactly_three = 0,0
 
+# Process all lines.
 while line = gets
-    chars = line.strip.chars
-    freq_map = {}
-    chars.each { |char|
-        freq_map[char] = 0 unless freq_map[char]
-        freq_map[char] += 1
+    memo = Hash.new(0)
+    # Build a frequency map for all characters in line.
+    freq_map = line.strip.chars.reduce() { |char|
+        memo[char] += 1
     }
+    # Increase counters if conditions met.
     exactly_two += 1 if freq_map.values.include?(2)
     exactly_three += 1 if freq_map.values.include?(3)
 end
