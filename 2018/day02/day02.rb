5357459ed15260759cd8cb34a4dd36a11efc4cfd
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-03 21:24:13 +0100

    Fix error in day 2. Optimizing code resulted in wrong semantics. It looked pretty.

--- 2018/day02/day02.rb
+++ 2018/day02/day02.rb
@@ -4,12 +4,13 @@ exactly_two, exactly_three = 0,0
 while line = gets
     memo = Hash.new(0)
     # Build a frequency map for all characters in line.
-    freq_map = line.strip.chars.reduce() { |char|
+    line.strip.chars.each { |char|
         memo[char] += 1
     }
+
     # Increase counters if conditions met.
-    exactly_two += 1 if freq_map.values.include?(2)
-    exactly_three += 1 if freq_map.values.include?(3)
+    exactly_two += 1 if memo.values.include?(2)
+    exactly_three += 1 if memo.values.include?(3)
 end
 
 puts exactly_two * exactly_three
