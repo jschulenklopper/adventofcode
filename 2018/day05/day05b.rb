a63b308f00349585a13c22858cfe3fde964c497f
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-05 22:18:57 +0100

    Compute lengths in map(), and then find minimal value.

--- 2018/day05/day05b.rb
+++ 2018/day05/day05b.rb
@@ -1,6 +1,7 @@
 line = gets.strip
 
 class String
+    # Shrink string by removing types of opposite polarity.
     def shrink
         result = []
 
@@ -15,19 +16,15 @@ class String
         result.join
     end
 
+    # Remove type (and other polarity) from string.
     def remove_type(type)
         self.gsub(type.upcase, "").gsub(type.downcase, "")
     end
 end
 
-min_length = Float::INFINITY
-
-# TODO Use min()
-("a".."z").each do |char|
-    result = line.remove_type(char).shrink
-    if result.length < min_length
-        min_length = result.length
-    end
+# Compute lengths of shrinked lines for all characters removed.
+lengths = ("a".."z").map do |char|
+    line.remove_type(char).shrink.length
 end
 
-puts min_length
+puts lengths.min
