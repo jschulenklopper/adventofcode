647fe44f90018beb1c3187d999eb03e826ebb291
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-08 17:12:22 +0100

    Add solution of day 8 part 2.

---
+++ 2018/day08/day08b.rb
@@ -0,0 +1,49 @@
+numbers = gets.strip.split.map(&:to_i)
+
+def read_node(numbers)
+    return nil if numbers == []
+
+    # Collect count of children and metadata entries.
+    nr_children, nr_metadata = numbers[0], numbers[1]
+
+    length = 2  # Length is minimal two: nr_children (1), nr_metadata (1).
+
+    # Collect child nodes, recursively, processing pieces of numbers.
+    children = []
+    nr_children.times do
+        child, size = read_node(numbers[length..-1])
+        length += size
+        children << child
+    end
+
+    # Collect metadata entries.
+    metadata = numbers[length, nr_metadata]
+    length += metadata.length
+
+    # Store children and metadata in a node.
+    node = { children: children, metadata: metadata}
+
+    # Return node and total size of numbers processed.
+    [node, length]
+end
+
+# Compute value of a node
+def value(node)
+    if node == nil
+        value = 0
+    elsif node[:children].empty?
+        value = node[:metadata].sum
+    else
+        value = 0
+        node[:metadata].each do |index|
+            value += value(node[:children][index-1]) if index > 0
+        end
+    end
+    value
+end
+
+# Read root node.
+node, _ = read_node(numbers)
+
+# Compute sum of all metadata entries.
+puts value(node)
