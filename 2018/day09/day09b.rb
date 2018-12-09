6e0938f342937fac5622d35b3ea578f636db495c
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-09 23:02:08 +0100

    Refactored day 9 part 2 to use linked list. Much better performance.

--- 2018/day09/day09b.rb
+++ 2018/day09/day09b.rb
@@ -1,14 +1,10 @@
 MAGIC_NUMBER = 23
 
-class Marble
+class Marble  # TODO Make this a simple Struct.
     attr_accessor :next, :prev, :value
 
     def initialize(v)
         @value = v
-    end
-
-    def to_s
-        @value.to_s
     end
 end
 
@@ -36,7 +32,6 @@ class Circle
 
         marble.prev = @current.prev
         marble.next = @current
-
         @current.prev.next = marble
         @current.prev = marble
 
@@ -51,44 +46,42 @@ class Circle
         times.times { @current = @current.prev }
     end
 
-    def delete  # Deletes marble at current position.
+    def delete  # Deletes marble at current position, return value.
         @count -= 1
         
         value = @current.value
-        @current.prev = @current.next
-        @current.next.prev = @current.prev
 
         @current = @current.next
+        @current.prev.prev.next = @current
+        @current.prev = @current.prev.prev
 
         value
     end
 end
 
 def high_score(nr_players, last_marble)
-    circle = [0]  # Circle starts with marble 0 in.
+    circle = Circle.new(0)  # Circle starts with marble 0 in.
     scores = Array.new(nr_players, 0)
     player = 0
 
-    (1..last_marble).each do |marble|
-        # puts "---" # Puts line for current round, before it starts.
-        # puts "[%2s] %s" % [player, circle.map { |c| "%2s" % c }.join(" ")]
-        # puts "     --"
 
+    (1..last_marble).each do |marble|
         player += 1
         player = player % nr_players
 
         if marble % MAGIC_NUMBER == 0
-            circle.rotate!(-7)
-            scores[player] += marble + circle.shift
+            circle.backward(7)
+            scores[player] += marble + circle.delete
         else
-            circle.rotate!(2)
-            circle.unshift(marble)
+            circle.forward(2)
+            circle.insert(marble)
         end
     end
     scores.max
 end
 
-line = gets
-nr_players, last_marble = line.match(/^(\d+) players.*worth (\d+) points$/).captures.map(&:to_i)
-last_marble *= 1
-puts high_score(nr_players, last_marble)
+while line = gets
+    next if line[0] == "#"
+    nr_players, last_marble = line.match(/^(\d+) players.*worth (\d+) points$/).captures.map(&:to_i)
+    puts high_score(nr_players, last_marble)
+end
