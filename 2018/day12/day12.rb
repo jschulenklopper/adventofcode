5d4e986914353cc07b6d14d4035aadf148ce16a2
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2018-12-16 20:35:27 +0100

    Finally, working solution for day 12 part 1.

---
+++ 2018/day12/day12.rb
@@ -0,0 +1,44 @@
+# Read starting state: row of pots.
+line = gets.match(/^initial state: ([#.]+)/).captures.first.chars
+state = line.map.with_index { |c, i| i if c == "#"}.compact
+
+# Read all the transformation rules.
+notes = []
+while line = gets
+  next if line.strip.empty?
+  temp_from, temp_to = line.match(/^(.{5}) =\> (.)$/).captures
+  from = temp_from.chars.map.with_index { |c,i| i-2 if c == "#" }
+  to = (temp_to == "#") ? 0 : nil
+  notes << [from, to]
+end
+
+# ---------------------
+
+# Applies all notes on current state returning new state.
+def apply_notes(notes, state)
+  new_state = []
+
+  # For all positions...
+  (state.min-2 .. state.max+2).each do |value|
+    # ... apply all the rules.
+    notes.each do |from, to|
+      # Build values from note that should be in state.
+      required_from = (value-2 .. value+2).each.map.with_index { |v, i| (from[i]) ? v : nil }
+      # Build list of values is state.
+      in_state = (value-2 .. value+2).each.map { |s| (state.include?(s)) ? s : nil }
+
+      if required_from == in_state
+        if to
+          new_state << value
+        end
+      end
+    end
+  end
+
+  new_state.sort
+end
+
+(1..20).each do |time|
+  state = apply_notes(notes, state)
+end
+puts state.sum
