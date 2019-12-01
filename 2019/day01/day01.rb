b2eab893075aadb923cc953d57a963a1baa0cda6
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2019-12-01 16:12:35 +0100

    Put fuel need calculation in proper method.

--- 2019/day01/day01.rb
+++ 2019/day01/day01.rb
@@ -1,4 +1,9 @@
 require '../../aoc'
 
+def fuel_needed(mass)
+  fuel = (mass.to_i / 3).floor - 2
+end
+
 lines = read_input("2019", "1")
-puts lines.map { |mass| (mass.to_i / 3).floor - 2}.sum
+
+puts lines.map { |mass| fuel_needed(mass) }.sum
