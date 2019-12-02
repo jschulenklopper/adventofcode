e9f6616f6c3e928a80bdb398769cbb575f2fae1b
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2019-12-02 22:22:03 +0100

    Don't need AoC library for day 1.

--- 2019/day01/day01.rb
+++ 2019/day01/day01.rb
@@ -1,9 +1,7 @@
-require '../../aoc'
-
 def fuel_needed(mass)
   fuel = (mass.to_i / 3).floor - 2
 end
 
-lines = read_input("2019", "1")
+lines = ARGF.readlines
 
 puts lines.map { |mass| fuel_needed(mass) }.sum
--- 2019/day01/day01b.rb
+++ 2019/day01/day01b.rb
@@ -1,11 +1,9 @@
-require '../../aoc'
-
 def fuel_needed(mass)
   fuel = (mass.to_i / 3).floor - 2
   # Increase fuel need for additional fuel needed.
   if fuel < 0 then 0 else fuel += fuel_needed(fuel) end
 end
 
-lines = read_input("2019", "1")
+lines = ARGF.readlines
 
 puts lines.map { |mass| fuel_needed(mass) }.sum
