ca39187029fe752519154952fc51f4ae22b26f02
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2019-12-05 13:08:11 +0100

    Fix issue that surprisingly got unnoticed and didn't change the number of correct passwords.

--- 2019/day04/day04.rb
+++ 2019/day04/day04.rb
@@ -3,14 +3,14 @@ RANGE = (248345..746315)  # TODO Read input file properly.
 count = 0
 
 # Rule: The value is within the range given in your puzzle input.
-RANGE.each do |number|
-  puts number
+RANGE.each do |num|
+  number = num.to_s
   # Rule: It is a six-digit number.
-  if number.to_s.length == 6 &&
+  if number.length == 6 &&
      # Rule : Two adjacent digits are the same (like 22 in 122345).
-     number.to_s.chars.uniq.length <= 5 &&
-     # Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-     number.to_s.chars.sort == number.to_s.chars
+     number.scan(/(.)\1{1,}/).length >= 1 && 
+     # Rule: going from left to right, the digits never decrease.
+     number.chars.sort == number.chars
   then   
     count += 1
   end
