287b33b756651c50913089b430fbee4234016d8c
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2019-12-07 20:09:48 +0100

    Make order of methods more logical.

--- 2019/day06/day06.rb
+++ 2019/day06/day06.rb
@@ -1,15 +1,6 @@
 # Read list of orbiting objects.
 orbits = ARGF.readlines.map { |line| line.strip.split(")") }
 
-# Compute cummulative length of all possible paths through tree.
-def count_paths(length_so_far, tree)
-  if tree[1]
-    length_so_far += tree[1].map { |t| count_paths(length_so_far+1, t) }.reduce(&:+)
-  else
-    length_so_far
-  end
-end
-
 # Transform list of orbits to tree.
 def tree(orbits, root)
   children = orbits.select { |o| o[0] == root }.map { |o| o[1] }
@@ -17,6 +8,15 @@ def tree(orbits, root)
     [root, children.map { |c| tree(orbits, c) } ]
   else
     [root]
+  end
+end
+
+# Compute cummulative length of all possible paths through tree.
+def count_paths(length_so_far, tree)
+  if tree[1]
+    length_so_far += tree[1].map { |t| count_paths(length_so_far+1, t) }.reduce(&:+)
+  else
+    length_so_far
   end
 end
 
