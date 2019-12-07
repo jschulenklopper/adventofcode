f0bf20c7bb96122965b0dfddfb9f0e8958dd6391
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2019-12-07 17:50:13 +0100

    Add solution for day 6 part 1, plus updated puzzle text.

---
+++ 2019/day06/day06.rb
@@ -0,0 +1,27 @@
+# Read list of orbiting objects.
+orbits = ARGF.readlines.map { |line| line.strip.split(")") }
+
+# Compute cummulative length of all possible paths through tree.
+def count_paths(length_so_far, tree)
+  if tree[1]
+    length_so_far += tree[1].map { |t| count_paths(length_so_far+1, t) }.reduce(&:+)
+  else
+    length_so_far
+  end
+end
+
+# Transform list of orbits to tree.
+def tree(orbits, root)
+  children = orbits.select { |o| o[0] == root }.map { |o| o[1] }
+  if children.length > 0
+    [root, children.map { |c| tree(orbits, c) } ]
+  else
+    [root]
+  end
+end
+
+def count(map)
+  
+end
+
+puts count_paths(0, tree(orbits, "COM"))
