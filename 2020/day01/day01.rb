8510927b8f683d01a00c415bb5d1545b8d731cb0
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-01 09:11:16 +0100

    Make solution for part 1 and 2 equal.

--- 2020/day01/day01.rb
+++ 2020/day01/day01.rb
@@ -1,7 +1,7 @@
 entries = ARGF.readlines.map(&:to_i)
 
 puts "part 1"
-puts entries.combination(2).select { |a,b| a + b == 2020 }.flatten.reduce(:*)
+puts entries.combination(2).select { |a| a.reduce(:+) == 2020 }.flatten.reduce(:*)
 
 puts "part 2"
-puts entries.combination(3).select { |a,b,c| a + b + c == 2020 }.flatten.reduce(:*)
+puts entries.combination(3).select { |a| a.reduce(:+) == 2020 }.flatten.reduce(:*)
