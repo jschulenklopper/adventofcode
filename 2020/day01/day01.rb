c9fec9a6ea1740de2262dbb1b533be99f130b413
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-01 09:46:44 +0100

    Make solution even more Ruby-esque.

--- 2020/day01/day01.rb
+++ 2020/day01/day01.rb
@@ -1,7 +1,7 @@
 entries = ARGF.readlines.map(&:to_i)
 
 puts "part 1"
-puts entries.combination(2).select { |a| a.reduce(:+) == 2020 }.flatten.reduce(:*)
+puts entries.combination(2).find { |a| a.sum == 2020 }.reduce(:*)
 
 puts "part 2"
-puts entries.combination(3).select { |a| a.reduce(:+) == 2020 }.flatten.reduce(:*)
+puts entries.combination(3).find { |a| a.sum == 2020 }.reduce(:*)
