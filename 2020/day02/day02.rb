2e1d320e02d57e43b89a08f036dc56d1ef867533
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-02 10:38:37 +0100

    Make code a bit more compact.

--- 2020/day02/day02.rb
+++ 2020/day02/day02.rb
@@ -1,21 +1,17 @@
-list = ARGF.readlines.map(&:strip)
-
-valid = list.select do |line|
-  policy, password = line.match(/(.+): (.+)/).captures
-  min, max, char = policy.match(/(\d+)-(\d+) (.)/).captures
-  # Return the number of valid passwords: `char` needs to occur min-max times.
-  password.count(char).between?(min.to_i,max.to_i)
-end
+list = ARGF.readlines.map { |line| line.match(/(.+): (.+)/).captures }
 
 puts "part 1"
-puts valid.count
-
-valid = list.select do |line|
-  policy, password = line.match(/(.+): (.+)/).captures
-  one, two, char = policy.match(/(\d+)-(\d+) (.)/).captures
-  # Return the number of valid passwords: only one of these conditions may be true.
-  (password[one.to_i-1] == char) ^ (password[two.to_i-1] == char)
+valid = list.count do |policy, password|
+  min, max, char = policy.match(/(\d+)-(\d+) (.)/).captures
+  # `char` needs to occur min-max times.
+  password.count(char).between?(min.to_i,max.to_i)
 end
+puts valid
 
 puts "part 2"
-puts valid.count
+valid = list.count do |policy, password|
+  one, two, char = policy.match(/(\d+)-(\d+) (.)/).captures
+  # Only one of these conditions may be true.
+  (password[one.to_i-1] == char) ^ (password[two.to_i-1] == char)
+end
+puts valid
