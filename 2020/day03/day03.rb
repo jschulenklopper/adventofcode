b4325950055d1a6793376b10780858e0aa9a80b3
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-03 10:14:29 +0100

    Add solution for day 3.

---
+++ 2020/day03/day03.rb
@@ -0,0 +1,35 @@
+forest = ARGF.readlines.map(&:strip).map(&:chars)
+TREE = "#"
+
+NR_COLUMNS = forest.first.length
+NR_ROWS = forest.length
+
+slope = [3, 1]  # Steps right, steps down.
+nr_trees = 0
+
+row, column = 0, 0
+until row >= NR_ROWS
+  nr_trees +=1 if forest[row][column % NR_COLUMNS] == TREE
+  column += slope.first
+  row += slope.last
+end
+
+puts "part 1"
+puts nr_trees
+
+slopes = [ [1,1], [3,1], [5,1], [7,1], [1,2] ]
+multiple_trees = 1
+
+slopes.each do |right, down|
+  nr_trees = 0
+  row, column = 0, 0
+  until row >= NR_ROWS
+    nr_trees +=1 if forest[row][column % NR_COLUMNS] == TREE
+    column += right
+    row += down
+  end
+  multiple_trees *= nr_trees
+end
+
+puts "part 2"
+puts multiple_trees
