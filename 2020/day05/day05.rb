7a2b1bc7e936084412fcf337a872fd6c50805c35
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-05 13:41:54 +0100

    Minor improvements.

--- 2020/day05/day05.rb
+++ 2020/day05/day05.rb
@@ -4,9 +4,9 @@ puts "part 1"
 
 # Build up list of seat IDs.
 seats = passes.map { |pass|
-   row = pass[0,7].gsub("F","0").gsub("B","1").to_i(2)
-   col = pass[7,3].gsub("L","0").gsub("R","1").to_i(2)
-   row * 8 + col
+   # ID is row * 8 + column.
+   pass[0,7].gsub(/[FB]/, 'F' => "0", 'B' => "1").to_i(2) * 8 +
+     pass[7,3].gsub(/[LR]/, 'L' => "0", 'R' => "1").to_i(2)
 }
 puts seats.max
 
@@ -15,4 +15,4 @@ puts "part 2"
 
 # Find missing seat by subtracting the seat list from a
 # complete range of all seats between the minimum and maximum.
-puts (seats.min .. seats.max).to_a - seats.sort
+puts (seats.min .. seats.max).to_a - seats
