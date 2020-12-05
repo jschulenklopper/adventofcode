a33c1dbcfa89018b5edc06fb004b7151eb3e5dcb
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-05 13:43:47 +0100

    Final minor improvement.

--- 2020/day05/day05.rb
+++ 2020/day05/day05.rb
@@ -1,18 +1,14 @@
 passes = ARGF.readlines.map(&:strip)
 
 puts "part 1"
-
-# Build up list of seat IDs.
 seats = passes.map { |pass|
-   # ID is row * 8 + column.
+   # Compute seat ID.
    pass[0,7].gsub(/[FB]/, 'F' => "0", 'B' => "1").to_i(2) * 8 +
-     pass[7,3].gsub(/[LR]/, 'L' => "0", 'R' => "1").to_i(2)
+   pass[7,3].gsub(/[LR]/, 'L' => "0", 'R' => "1").to_i(2)
 }
 puts seats.max
 
-
 puts "part 2"
-
-# Find missing seat by subtracting the seat list from a
-# complete range of all seats between the minimum and maximum.
+# Find missing seat by subtracting the seat list from the
+# range of all seats between the minimum and maximum seat.
 puts (seats.min .. seats.max).to_a - seats
