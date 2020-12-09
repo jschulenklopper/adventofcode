92685d53166009327212664b51a0b2a168b202e7
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-09 22:25:39 +0100

    Add (proper, refactored) solution for day 8 (incl. part 2).

--- 2020/day08/day08.rb
+++ 2020/day08/day08.rb
@@ -5,43 +5,71 @@ program = ARGF.readlines.map { |line|
   [instruction[0], instruction[1].to_i]
 }
 
-# Run one instruction of program.
-# TODO Alternatively, make a separate function to run whole program,
-# although manipulation or inspection at run-time is more difficult.
-def run(program, pc, acc)
-  puts "run(program, %i, %i)" % [pc, acc]
+# Run one instruction of program, return [program counter, accumulator].
+def run_instruction(program, pc, acc)
   operation, argument = program[pc]
 
-  p [operation, argument]
-
   case operation
-  when "acc"
-    acc += argument
-    pc += 1
-  when "jmp"
-    pc += argument
-  when "nop"
-    pc += 1
+    when "acc" then acc += argument; pc += 1
+    when "jmp" then pc += argument
+    when "nop" then pc += 1
   end
   
   [pc, acc]
 end
 
-# Set up context for program to run.
-pc = 0
-accumulator = 0
+# Run complete program, return [accumulator, exit code].
+def run(program)
+  # Set up context.
+  pc = 0
+  acc = 0
 
-# Set up variables to notice if we're in a loop.
-in_loop = false
-seen = Array.new
+  # Set up variable to notice if we're in a loop.
+  seen = []
 
-while not in_loop do
-  pc, accumulator = run(program, pc, accumulator)
-  # Exit loop if we've seen pc before.
-  if seen.include?(pc)
-    puts accumulator
-    break
+  while pc.between?(0,program.length - 1) do
+    pc, acc = run_instruction(program, pc, acc)
+    if seen.include? pc
+      pc = -1  # Invalid value to halt program.
+    end
+    seen << pc
   end
-  seen << pc
+  
+  exit_code = pc == -1 ? -1 : 0
+  [acc, exit_code]
 end
 
+# Generate list of mutated programs.
+def mutates(program)
+  programs = []
+
+  # Maximum number of mutations is number of instructions.
+  program.length.times do |i|
+    # Make a deep dup of program. FIXME This is ugly.
+    prgm = program.dup.map(&:clone)
+    # Make mutation of instruction at i.
+    if prgm[i][0] == "nop"
+      prgm[i][0] = "jmp"
+    elsif prgm[i][0] == "jmp"
+      prgm[i][0] = "nop"
+    else
+      next
+    end
+    # Add mutated program to list of mutations.
+    programs << prgm 
+  end
+  programs
+end
+
+puts "part 1"
+
+acc, exit_code = run(program.dup)
+puts acc
+
+puts "part 2"
+
+# Run all program mutations.
+mutates(program).each do |program|
+  acc, exit_code = run(program)
+  puts acc if exit_code == 0
+end
