0144e1b7ff4056d50bf2afdee2109b80418569a9
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-09 15:38:57 +0100

    Add solution for day 7 part 2.

--- 2020/day07/day07.rb
+++ 2020/day07/day07.rb
@@ -13,15 +13,31 @@ end
 
 puts "part 1"
 
-def contain(bag)
+# Count the number of bags that can contain bag.
+def can_contain(bag)
   # Find bags that can be inside bag.
-  bags = $rules.select { |outside, inside|
-    inside.select { |nr, color| color == bag }.length > 0
+  bags = $rules.select { |_, inside|
+    inside.select { |_, color| color == bag }.length > 0
   }.map(&:first)
+
   # Find bags that can be inside all those bags.
-  inside = bags.map { |color| contain(color) }
-  # Add those two lists.
+  inside = bags.map { |color| can_contain(color) }
+
+  # Add those two lists. TODO Return just the length.
   (bags + inside.flatten).uniq
 end
 
-puts contain("shiny gold").length
+puts can_contain("shiny gold").length
+
+puts "part 2"
+
+# Count the number of bags that can be inside bag.
+def count_contain(bag)
+  # Find bags that can be inside bag.
+  bags = $rules.select { |outside, _| (bag == outside) }.map(&:last).first
+
+  bags.map.reduce(1) { |sum, bag| sum += bag[0].to_i * count_contain(bag[1]) } if bags
+end
+
+# Subtract 1 to 
+puts count_contain("shiny gold") - 1
