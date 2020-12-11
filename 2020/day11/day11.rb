cd07648cf23ba2ff2fd09313f2fe99e9325cc4d2
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-11 23:06:50 +0100

    Reuse methods for both part 1 and 2.

--- 2020/day11/day11.rb
+++ 2020/day11/day11.rb
@@ -7,18 +7,7 @@ ARGF.readlines.each.with_index { |row, r|
   }
 }
 
-def count_neighbors(layout, key)
-  deltas = [ [-1,-1], [-1, 0], [-1, +1],
-             [ 0,-1],          [ 0, +1],
-             [ 1,-1], [ 1, 0], [ 1, +1] ]
-  deltas.select { |delta_r, delta_c|
-    new_key = [key[0] + delta_r, key[1] + delta_c] 
-    layout[ new_key ] == "#"
-  }.count
-end
-
-# TODO This can be merged into count_neighbors().
-def count_in_sight(layout, key)
+def count_in_sight(layout, key, just_neighbors)
   deltas = [ [-1,-1], [-1, 0], [-1, +1],
              [ 0,-1],          [ 0, +1],
              [+1,-1], [+1, 0], [+1, +1] ]
@@ -31,46 +20,31 @@ def count_in_sight(layout, key)
       # Apply delta (movement).
       new_key = [ new_key[0] + delta[0], new_key[1] + delta[1] ]
       # Check whether border or neighbor has been found.
-      border = true if layout[new_key] == nil || layout[new_key] == "L"
+      border = true if layout[new_key] == nil || layout[new_key] == "L" || just_neighbors
       found = true if layout[new_key] == "#"
     end
     found
   }.count
 end
 
-def round_part_1(layout)
+def one_round(layout, threshold, just_neighbors)
   new_layout = Hash.new
 
   layout.each do |key, seat|
-    occupied = count_neighbors(layout,key) unless layout[key] == "."
+    occupied = count_in_sight(layout,key, just_neighbors) unless layout[key] == "."
     case seat
       when "L" then new_layout[key] = (occupied == 0) ? "#" : layout[key]
-      when "#" then new_layout[key] = (occupied >= 4) ? "L" : layout[key]
+      when "#" then new_layout[key] = (occupied >= threshold) ? "L" : layout[key]
       else          new_layout[key] = layout[key]
     end
   end
   new_layout
 end
-
-def round_part_2(layout)
-  new_layout = Hash.new
-
-  layout.each do |key, seat|
-    occupied = count_in_sight(layout,key) unless layout[key] == "."
-    case seat
-      when "L" then new_layout[key] = (occupied == 0) ? "#" : layout[key]
-      when "#" then new_layout[key] = (occupied >= 5) ? "L" : layout[key]
-      else          new_layout[key] = layout[key]
-    end
-  end
-  new_layout
-end
-
 puts "part 1"
 
 layout = $layout
 while true
-  new_layout = round_part_1(layout)
+  new_layout = one_round(layout, 4, true)
   break if layout == new_layout
   layout = new_layout
 end
@@ -81,7 +55,7 @@ puts "part 2"
 
 layout = $layout
 while true
-  new_layout = round_part_2(layout)
+  new_layout = one_round(layout, 5, false)
   break if layout == new_layout
   layout = new_layout
 end
