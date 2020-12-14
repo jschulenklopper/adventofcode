6b3525aaeb096af97e88882853e65a2c36315828
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-14 08:51:31 +0100

    Add better solution for part 1; part 2 underway.

--- 2020/day13/day13.rb
+++ 2020/day13/day13.rb
@@ -1,9 +1,52 @@
-depart = ARGF.readline.to_i
+$departure = ARGF.readline.to_i
 buses = ARGF.readline.split(",").map(&:to_i)
+$buses = buses.map.with_index { |id, i| [i, id] if id != 0 }.compact
 
 puts "part 1"
-waits = buses.map { |id| id == 0 ? nil : -(depart % id) + id}
-shortest = waits.reject { |w| w == nil }.min
 
-index = waits.find_index(shortest)
-puts buses[index] * waits[index]
+# Find wait times for all the buses from departure time.
+wait_times = $buses.map { |i, id| [ i, id, -$departure % id ] }
+# Find the lowest wait time (the third entry in wait_times).
+shortest = wait_times.sort { |a,b| a[2] <=> b[2]}.first
+
+# Multiple lowest wait time with bus ID.
+puts shortest[1] * shortest[2]
+
+puts "part 2"
+
+class Array
+  def lcm
+    self.reduce(1, :lcm)
+  end
+end
+
+# Compute next departure after t of bus with id at index.
+# TODO Fix this since $buses has a different structure now.
+def next_depart(t, index, id)
+  (id == 0) ? 0 : (t + index) % id
+end
+
+puts $buses.to_s
+# TODO Make list of (index, id) pairs. That saves handling the irrelevant buses.
+p $buses.map.with_index { |id, i| [i, id] if id != 0 }.compact
+
+t = 0
+step = 1
+
+loop do
+  puts "\nt: %i" % t
+  puts "step: %i" % step
+
+  # Compute departure times for all buses from time t.
+  departures = $buses.map.with_index { |id, i|
+    next_depart(t, i, id)
+  }
+  puts "departures: %s" % departures.to_s
+
+  # We're done if all departures are 0.
+  break if departures.all?(0)
+
+  t += step
+end
+puts t
+
