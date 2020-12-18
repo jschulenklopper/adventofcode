e2bb86db0bb9fbc9e71ee0d3452f24e221e0b863
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-18 11:56:24 +0100

    Add solution for day 18, part 2.

--- 2020/day18/day18.rb
+++ 2020/day18/day18.rb
@@ -1,13 +1,22 @@
-expressions = ARGF.readlines.select { |l| l[0] != "#" }.map(&:strip)
+    expressions = ARGF.readlines.map(&:strip)
 
-class Integer
-  def %(other_integer)
-    self + other_integer
-  end
-end
+    class Integer
+      def %(operand)
+        self + operand
+      end
+      def **(operand)
+        self + operand
+      end
+    end
 
-puts "part 1"
-puts expressions.reduce(0) { |sum, expression|
-  modified_expression = expression.gsub("+", "%")
-  sum += eval(modified_expression)
-}
+    puts "part 1"
+    puts expressions.reduce(0) { |sum, expression|
+      mod_expression = expression.gsub("+", "%")
+      sum += eval(mod_expression)
+    }
+
+    puts "part 2"
+    puts expressions.reduce(0) { |sum, expression|
+      mod_expression = expression.gsub("+", "**")
+      sum += eval(mod_expression)
+    }
