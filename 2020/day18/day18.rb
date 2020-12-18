7d7276a0844be0b6f5d98583e3bf3cd9f02d496e
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-18 12:59:43 +0100

    Improve documentation.

--- 2020/day18/day18.rb
+++ 2020/day18/day18.rb
@@ -1,22 +1,24 @@
-    expressions = ARGF.readlines.map(&:strip)
+expressions = ARGF.readlines.map(&:strip)
 
-    class Integer
-      def %(operand)
-        self + operand
-      end
-      def **(operand)
-        self + operand
-      end
-    end
+class Integer
+  def %(operand)
+    # `%` has the same precedence as `*`.
+    self + operand
+  end
+  def **(operand)
+    # `**` has higher precedence than `*`.
+    self + operand
+  end
+end
 
-    puts "part 1"
-    puts expressions.reduce(0) { |sum, expression|
-      mod_expression = expression.gsub("+", "%")
-      sum += eval(mod_expression)
-    }
+puts "part 1"
+puts expressions.reduce(0) { |sum, expression|
+  mod_expression = expression.gsub("+", "%")
+  sum += eval(mod_expression)
+}
 
-    puts "part 2"
-    puts expressions.reduce(0) { |sum, expression|
-      mod_expression = expression.gsub("+", "**")
-      sum += eval(mod_expression)
-    }
+puts "part 2"
+puts expressions.reduce(0) { |sum, expression|
+  mod_expression = expression.gsub("+", "**")
+  sum += eval(mod_expression)
+}
