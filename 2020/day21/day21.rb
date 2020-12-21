7ca6734a657ae5e74cd1aae08b1f068339fc5b8f
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-21 21:16:37 +0100

    Add solution for part 2.

--- 2020/day21/day21.rb
+++ 2020/day21/day21.rb
@@ -1,7 +1,6 @@
 # Read list of foods (and remove characters we don't need).
 lines = ARGF.readlines.map { |line| line.gsub(/\(|\)|,/,"").strip }
-foods = []
-suspect = Hash.new { |hash, k| hash[k] = [] }
+foods, suspect = [], {}
 
 puts "part 1"
 
@@ -10,18 +9,28 @@ lines.each do |line|
   # Add ingredients and allergens to store of food items.
   foods << [ingredients, allergens]
 
-  allergens.each do |allergen|
-    if suspect[allergen].length > 0
-      suspect[allergen] = suspect[allergen] & ingredients
-    else
-      suspect[allergen] = ingredients
-    end
+  allergens.each do |a|
+    # Intersect ingredient list if suspected earlier.
+    suspect[a] = (suspect[a]) ? suspect[a] & ingredients : ingredients
   end
 end
 
 ingredients = foods.map(&:first).flatten.uniq
 
-# Safe ingredients are those that aren't in a suspect list.
+# Safe ingredients are ingredients not in a suspect list.
 safe = ingredients - suspect.values.flatten
-# Count how often those safe ingredients occur in food items.
+# Count how often safe ingredients occur in food ingredient list.
 puts safe.sum { |ingredient| foods.select { |i,_| i.include?(ingredient) }.count }
+
+puts "part 2"
+
+until suspect.map { |_, ingredients| ingredients.length }.uniq.length == 1
+  # Find ingredients to strike: ingredients that are the only suspected one.
+  to_strike = suspect.values.select { |i| i.length == 1 }.flatten
+  suspect.each do |a, i|
+    to_strike.each { |strike| suspect[a] -= [strike] if i.length > 1 }
+  end
+end
+
+# Order suspect ingredients on allergen name.
+puts suspect.keys.sort.map { |a,_| suspect[a] }.join(",")
