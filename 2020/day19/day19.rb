74c2d6f864e618254067de12c43fcef9f10b9a27
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-23 23:56:20 +0100

    Add solution for part 2 of day 19.

--- 2020/day19/day19.rb
+++ 2020/day19/day19.rb
@@ -1,11 +1,17 @@
-def regexp_for(expression, root = false)
+def regexp_for(id, expression, root = false)
   if match = expression.match(/([a-z])/)
     regexp = match[1]
   elsif expression.include?("|")
-    regexp = "(%s|%s)" % expression.split("|").map { |part| regexp_for(part) }
+    regexp = "(%s|%s)" % expression.split("|").map { |part| regexp_for(id, part) }
   else expression.include?(" ")
     parts = expression.split(" ")
-    regexp = "(%s)" % parts.map { |p| regexp_for($rules[p.to_i]) }.join
+    if parts.include?(id.to_s)
+      regexp = "(?<r#{id}>%s)" % parts.map do |p|
+        p.to_i == id ? "\\g<r#{p.to_i}>?" : regexp_for(p.to_i, $rules[p.to_i])
+      end.join
+    else  
+      regexp = "(%s)" % parts.map { |p| regexp_for(p.to_i, $rules[p.to_i]) }.join
+    end
   end
   root ? "^%s$" % regexp : regexp
 end
@@ -20,5 +26,20 @@ while line = ARGF.readline.strip
 end
 
 messages = ARGF.readlines.map(&:strip)
-regexp_0 = Regexp.new(regexp_for($rules[0], true))
+
+# Build regular expression for rule 0.
+regexp_0 = Regexp.new(regexp_for(0, $rules[0], true))
+# Count number of valid (matching) messages.
+puts messages.count { |msg| msg.match(regexp_0) }
+
+puts "part 2"
+
+# Change rules 8 and 11 according to instructions.
+$rules[8] = "42 | 42 8"
+$rules[11] = "42 31 | 42 11 31"
+
+# Rebuild regular expression for rule 0.
+regexp_0 = Regexp.new(regexp_for(0, $rules[0], true))
+
+# Count number of valid (matching) messages.
 puts messages.count { |msg| msg.match(regexp_0) }
