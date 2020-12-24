8cc77dc0964d200f34bac1ffbfca6e0f5ebc1b9b
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-24 17:36:28 +0100

    Remove some comments.

--- 2020/day23/day23.rb
+++ 2020/day23/day23.rb
@@ -1,37 +1,32 @@
-cups = ARGF.readline.strip.chars.map(&:to_i)
+$cups = ARGF.readline.strip.chars.map(&:to_i)
+$lowest, $highest = [ $cups.min, $cups.max ]
+$length = $cups.length
+cups = $cups.dup
 
-current_index = 0
-length = cups.length
-lowest, highest = [ cups.min, cups.max ]
-
-puts "part 1"
-100.times do |i|
+def one_move(cups, current_index)
   label = cups[current_index]
 
-  pick_up = (1..3).map { |i| cups[(current_index + i) % length] }
+  pick_up = (1..3).map { |i| cups[(current_index + i) % $length] }
   pick_up.each { |cup| cups.delete(cup) }
 
-  destination = label - 1
-  destination = highest if destination < lowest
+  destination = (label <= $lowest) ? $highest : label - 1
 
-  # If this would select one of the cups that was just picked up,
-  # the crab will keep subtracting one until it finds a cup that
-  # wasn't just picked up.
   while pick_up.include?(destination)
-    destination -= 1
-
-    # If at any point in this process the value goes below the
-    # lowest value on any cup's label, it wraps around to the
-    # highest value on any cup's label instead.
-    if destination < lowest
-      destination = highest
-    end
+    destination = (destination <= $lowest) ? $highest : destination - 1 
   end
   
-  destination = highest if destination < lowest
   destination_index = cups.find_index(destination)
   cups.insert(destination_index + 1, pick_up[0], pick_up[1], pick_up[2])
-  current_index = (cups.find_index(label) + 1) % length
+  current_index = (cups.find_index(label) + 1) % $length
+
+  [cups, current_index]
+end
+
+puts "part 1"
+
+current_index = 0
+100.times do |i|
+  cups, current_index = one_move(cups, current_index)
 end
 
 one_at = cups.find_index(1)
