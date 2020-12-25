82559e167e9208baf032516a93b740719e87a63e
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-25 14:19:48 +0100

    Refactor step into separate lambda function.

--- 2020/day25/day25.rb
+++ 2020/day25/day25.rb
@@ -3,25 +3,25 @@ door_key = ARGF.readline.to_i
 
 SUBJECT, MOD = 7, 20201227
 
-# The card transforms the subject number of 7 according to the
-# card's secret loop size. The result is called the card's public key.
+f_step = lambda { |subject, card| (card * subject) % MOD }
+
+puts "part 1"
+
 card_subject, card_loop = 1, 0
 until card_subject == card_key
   card_loop += 1
-  card_subject = (card_subject * SUBJECT) % MOD
+  card_subject = f_step.call(SUBJECT, card_subject)
 end
 
 door_subject, door_loop = 1, 0
 until door_subject == door_key
   door_loop += 1
-  door_subject = (door_subject * SUBJECT) % MOD
+  door_subject = f_step.call(SUBJECT, door_subject)
 end
 
-# The card transforms the subject number of the door's public key according
-# to the card's loop size. The result is the encryption key.
 encryption_key = 1
 card_loop.times do
-  encryption_key = (encryption_key * door_key) % MOD
+  encryption_key = f_step.call(door_key, encryption_key)
 end
 
 puts encryption_key
