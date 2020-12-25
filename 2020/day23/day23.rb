4b9099f98a44ba39f1211d80444c7a01407d35b3
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2020-12-25 22:06:43 +0100

    Add solution for part 2 of day 23, using hash in a smart way.

--- 2020/day23/day23.rb
+++ 2020/day23/day23.rb
@@ -1,33 +1,56 @@
 $cups = ARGF.readline.strip.chars.map(&:to_i)
-$lowest, $highest = [ $cups.min, $cups.max ]
-$length = $cups.length
-cups = $cups.dup
 
-def one_move(cups, current_index)
-  label = cups[current_index]
+def one_move(circle, current, lowest, highest)
+  # Pick up three cups, remove (bypass) from circle.
+  pick_up = [ circle[current], circle[circle[current]], circle[circle[circle[current]]] ]
+  circle[current] = circle[pick_up.last]
 
-  pick_up = (1..3).map { |i| cups[(current_index + i) % $length] }
-  pick_up.each { |cup| cups.delete(cup) }
-
-  destination = (label <= $lowest) ? $highest : label - 1
-
+  # Select destination cup.
+  destination = (current <= lowest) ? highest : current - 1
   while pick_up.include?(destination)
-    destination = (destination <= $lowest) ? $highest : destination - 1 
+    destination = (destination <= lowest) ? highest : destination - 1
   end
-  
-  destination_index = cups.find_index(destination)
-  cups.insert(destination_index + 1, pick_up[0], pick_up[1], pick_up[2])
-  current_index = (cups.find_index(label) + 1) % $length
 
-  [cups, current_index]
+  # Place picked up cups after destination.
+  circle[pick_up[2]] = circle[destination]
+  circle[destination] = pick_up[0]
+
+  # Select new current cup: next cup clockwise.
+  current = circle[current]
+
+  [circle, current]
 end
 
 puts "part 1"
 
-current_index = 0
+# Circle of numbers as key, and next number as value.
+circle = {}
+$cups.each.with_index { |c,i| circle[c] = $cups[(i+1) % $cups.length]  }
+lowest, highest = $cups.min, $cups.max
+
+# Current number.
+current = $cups[0]
+
 100.times do |i|
-  cups, current_index = one_move(cups, current_index)
+  circle, current = one_move(circle, current, lowest, highest)
 end
 
-one_at = cups.find_index(1)
-puts cups.rotate(one_at)[1..-1].join
+start = 1
+puts (circle.length-1).times.map { |str, _| str += start; start = circle[start] }.join
+
+puts "part 2"
+
+# Build circle from inital cups, and extend to 1000000.
+circle = {}
+($cups.max+1 .. 1000000).each { |cup| $cups << cup }
+$cups.each.with_index { |c,i| circle[c] = $cups[(i+1) % $cups.length]  }
+lowest, highest = $cups.min, $cups.max
+
+# Current number.
+current = $cups[0]
+
+10000000.times do |i|
+  circle, current = one_move(circle, current, lowest, highest)
+end
+
+puts circle[1] * circle[circle[1]]
