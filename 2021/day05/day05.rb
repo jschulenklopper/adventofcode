2c51d2b2f22a0f41b5a61be69103290906cdc778
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2021-12-18 22:57:34 +0100

    Add solution for day 5.

---
+++ 2021/day05/day05.rb
@@ -0,0 +1,43 @@
+lines = ARGF.readlines.map(&:strip)
+vents = Array.new
+
+lines.each do |line|
+  c = line.match(/(\d+),(\d+) -> (\d+),(\d+)/).captures.map(&:to_i)
+  vents << [c[0], c[1], c[2], c[3]]
+end
+
+grid = Hash.new { |k,v| 0 }
+
+# Process the horizontal lines.
+vents.each do |vent|
+  fx, fy, tx, ty = vent
+  next unless fx == tx || fy == ty
+
+  if fx > tx then fx,tx = tx,fx end
+  if fy > ty then fy,ty = ty,fy end
+  (fx .. tx).each do |x|
+    (fy .. ty).each do |y|
+      grid[ [x,y] ] += 1
+    end
+  end
+end
+
+puts "part 1"
+puts grid.values.select { |v| v > 1 }.count
+
+# Process the diagonal lines.
+vents.each do |vent|
+  fx, fy, tx, ty = vent
+  next if (fx == tx || fy == ty)
+
+  if fx > tx then dx = -1 else dx = 1 end
+  if fy > ty then dy = -1 else dy = 1 end
+  ((fx-tx).abs + 1).times do
+    grid[ [fx,fy] ] += 1
+    fx += dx
+    fy += dy
+  end
+end
+
+puts "part 2"
+p grid.values.select { |v| v > 1 }.count
