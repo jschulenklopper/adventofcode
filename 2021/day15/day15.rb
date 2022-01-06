f54a8351319001559997480dfdb8959861e32804
Jochem Schulenklopper <j.schulenklopper@gmail.com>
2022-01-06 17:01:49 +0100

    Add solution for day 15.

---
+++ 2021/day15/day15.rb
@@ -0,0 +1,78 @@
+grid = Hash.new { |hash,key| hash[key] = 0 }
+ARGF.readlines.each.with_index { |line,x| line.strip.chars.each.with_index { |r,y| grid[ [y,x] ] = r.to_i } }
+
+def remaining(grid, from, to)
+  # Compute Manhattan distance, using 5 as an estimate for average cost.
+  ((from[0] - to[0]).abs + (from[1] - to[1]).abs) * 5
+end
+
+def neighbors(grid, node)
+  # Calculate neighbors of node in grid.
+  [ [-1,0], [0,-1], [0,+1], [+1,0] ]  # Valid deltas for neighbors of `node`.
+    .map { |d| [ node[0]+d[0], node[1]+d[1] ] }
+    .filter { |k| grid.key?(k) }
+end
+
+def expand(grid)
+  expanded_grid = {}
+  size_x, size_y = grid.keys.sort.max[0]+1, grid.keys.sort.max[1]+1
+
+  5.times do |dx|
+    5.times do |dy|
+      grid.each do |key,_|
+        new_key = [dx * size_x + key[0], dy * size_y + key[1]]
+        new_value = (grid[ [key[0],key[1]] ] + dx + dy)
+        new_value -= 9 if new_value > 9
+        expanded_grid[ new_key ] = new_value
+      end
+    end
+  end
+
+  expanded_grid
+end
+
+def cheapest_path(grid, start, goal)
+  # Prepare queue and visited data structures.
+  # Queue is a list of grid positions, actual and expected costs.
+  queue = [ { :key => start, :cost => 0, :remaining => remaining(grid, start, goal) } ]
+  # Visited is a grid (hash) of grid positions and actual costs to get there.
+  visited = { start => 0 } # { node => cost }
+
+  # Loop until queue empty; take front of queue.
+  while node = queue.shift do
+    # puts "queue.length: %6i, visited.length: %6i" % [queue.length, visited.length]
+
+    # Add to visited list
+    visited[node[:key]] = node[:cost]
+
+    # Get neighbors that haven't been visited yet.
+    neighbors = neighbors(grid, node[:key]).filter { |key| ! visited.keys.include?(key) }
+
+    neighbors.each do |neighbor|
+      # Compute cost of getting here. 
+      cost = grid[neighbor] + node[:cost]
+
+      # Compute expected cost of reaching goal.
+      remaining = remaining(grid, neighbor, goal)
+
+      # Add to queue, if it's not already in there.
+      new_node = { :key => neighbor, :cost => cost, :remaining => remaining }
+      queue << new_node unless queue.count { |q| q[:key] == new_node[:key] } > 0
+    end
+
+    # Sort queue on total and expected cost.
+    queue.sort! { |node1, node2|
+      node1[:cost] <=> node2[:cost]
+    }
+  end
+
+  # Return the cost of the goal node.
+  visited[goal] if visited[goal]
+end
+
+puts "part 1"
+puts cheapest_path(grid, [0,0],  grid.keys.sort.max)
+
+puts "part 2"
+expanded_grid = expand(grid)
+puts cheapest_path(expanded_grid, [0,0],  expanded_grid.keys.sort.max)
